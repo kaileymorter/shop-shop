b466ee1feb572c5fb7a8c5adedc96a15a678db7d
Spartacus <k_morter@outlook.com>
2020-09-21 12:58:09 -0600

    added cart tests and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,8 +4,15 @@ import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from "./actions";
+//import { triggerAsyncId } from 'async_hooks';
 
 export const reducer = (state, action) => {
     switch(action.type) {
@@ -26,6 +33,55 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 currentCategory: action.currentCategory
+            };
+        // if action type value is the value of `ADD_TO_CART`
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+        //if action type value is the value of `ADD_MULTIPLE_TO_CART`
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products]
+            };
+        //if action type value is the value of `REMOVE_FROM_CART`
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            }
+        //if action type value is the value of `UPDATE_CART_QUANTITY`
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if(action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product
+                })
+            }
+        //if action type value is the value of `CLEAR_CART`
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+        //if action type value is the value of `TOGGLE_CART`
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
             };
         //if it's none of these actions, do not update state at all and keep things the same!
         default:
