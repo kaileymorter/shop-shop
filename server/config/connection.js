48efd03b3db18e10c97a7f2c541971400cdc5ebf
Spartacus <k_morter@outlook.com>
2020-09-22 11:00:26 -0600

    added MongoDB atlas connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+MONGODB_URI = `mongodb+srv://kaileymorter:${process.env.DB_PASS}@cluster1.qnq7i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
