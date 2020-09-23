ba213771780b4b8d020bcf5225d49fbe29268736
Spartacus <k_morter@outlook.com>
2020-09-23 14:26:55 -0600

    added a Redux store to the app

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -12,6 +11,8 @@ import Success from './pages/Success';
 import Nav from "./components/Nav";
 // import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+import { Provider } from 'react-redux'
+import store from './utils/store'
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -30,7 +31,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
               <Switch>
                 <Route exact path="/" component={Home} />
@@ -41,7 +42,7 @@ function App() {
                 <Route exact path="/success" component={Success} />
                 <Route component={NoMatch} />
               </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
