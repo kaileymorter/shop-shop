2e4c9c07d94387170b176426b0f18ee1527b53d4
Spartacus <k_morter@outlook.com>
2020-09-23 13:43:19 -0600

    edited reducer file

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -18,7 +18,7 @@ const initialState = {
     currentCategory: '',
 }
 
-export const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
     switch(action.type) {
         //if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
         case UPDATE_PRODUCTS:
@@ -93,6 +93,4 @@ export const reducer = (state = initialState, action) => {
     }
 }
 
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState)
-}
+export default reducer;
